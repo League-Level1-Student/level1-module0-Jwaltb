b2d7b27127a357c8a02c6dd95ada2596ae0fa6cc
league <league@imac.attlocal.net>
2018-06-18 19:50:09 -0700

    drum DrumKit MyFirstSwing PhotoQuiz

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -1,8 +1,8 @@
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2017
  *    Level 1
  */
-
 
 import java.awt.Component;
 import java.net.MalformedURLException;
@@ -13,40 +13,56 @@ import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 
 public class PhotoQuiz {
 
 	public static void main(String[] args) throws Exception {
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
-                quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
+		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // This will make sure the program exits when you
+																	// close the window
 
-		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+		// 1. find an image on the internet, and put its URL in a String variable (from
+		// your browser, right click on the image, and select “Copy Image Address”)
+		String image = "https://www.ocf.berkeley.edu/~sather/wp-content/uploads/2018/01/food--1200x600.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component c;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		c = createImage(image);
 		// 4. add the image to the quiz window
-
+		quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String question1 = JOptionPane.showInputDialog("How many dishes are in the picture?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if (question1.equals("8")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		} else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
 
-		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+		// 9. remove the component from the quiz window (you may not see the effect of
+		// this until step 12)
+		quizWindow.remove(c);
 		// 10. find another image and create it (might take more than one line of code)
-
+		String image2 = "https://www.simplepastimes.com/prod_images_blowup/70018-candy-jars.jpg";
 		// 11. add the second image to the quiz window
-
+		Component d;
+		d = createImage(image2);
 		// 12. pack the quiz window
-
+		quizWindow.add(d);
+		quizWindow.pack();
 		// 13. ask another question
+		String question2 = JOptionPane.showInputDialog("How many swirlypops are there?");
+		if (question2.equals("5")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		} else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
 
+		}
 		// 14+ check answer, say if correct or incorrect, etc.
 
 	}
@@ -60,10 +76,6 @@ public class PhotoQuiz {
 
 	/* OPTIONAL */
 	// *14. add scoring to your quiz
-	// *15. make something happen when mouse enters image (imageComponent.addMouseMotionListener()) 
+	// *15. make something happen when mouse enters image
+	// (imageComponent.addMouseMotionListener())
 }
-
-
-
-
-
